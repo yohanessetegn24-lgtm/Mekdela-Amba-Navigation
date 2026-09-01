776773d6ea03cc12632d1871ac85fb74fdf7e89b
Yohanes Setegn <yohanessetegn24@gmail.com>
2026-09-01 03:13:34 -0700

    api.js edit

--- MekdelaCampusNav/src/services/api.js
+++ MekdelaCampusNav/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // ባክኤንዱ በ 5030 (HTTP) ላይ ስለሚሰራ እዚህ ጋር እናስተካክላለን
 const api = axios.create({
-  baseURL: 'http://localhost:5030/api', 
+  baseURL: 'https://mekdela-amba-navigation.onrender.com/api', 
 });
 
 export default api;
