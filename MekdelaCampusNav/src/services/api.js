f7fc657337710f668b03a1d0a8a438e3e40a4e1b
yohanessetegn24-lgtm <yohanessetegn24@gmail.com>
2026-09-09 01:33:38 -0700

    Update API base URL to production endpoint

--- MekdelaCampusNav/src/services/api.js
+++ MekdelaCampusNav/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL|| 'http://localhost:5030/api', // የAPI መለያየት አድራሻ
+  baseURL: 'https://maumap.runasp.net/api',
 });
 
 export default api;
